3f2e9c444d2ac7279583fcf22c7a7b74081a049b
Geervani22 <92290343+Geervani22@users.noreply.github.com>
2021-12-22 11:22:13 +0530

    Add files via upload

---
+++ DemoCode.java
@@ -0,0 +1,11 @@
+package com.org.hcl;
+
+public class DemoCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" Code is working ......");
+System.out.println(" Code is compiling .....");
+	}
+
+}
